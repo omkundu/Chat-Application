eedf01d3a8f944833253b641fac0312e5e84e65d
omkundu <omkundu156@gmail.com>
2023-08-13 11:38:23 +0530

    chat-app-initial-data-pushed

---
+++ index.js
@@ -0,0 +1,29 @@
+import express from "express"
+import http from "http"
+import {Server} from "socket.io" 
+
+const app=express();
+const PORT=4000;
+const httpServer=http.createServer(app)
+const io=new Server(httpServer)
+
+import path from "path"
+import {fileURLToPath} from "url"
+const __filename=fileURLToPath(import.meta.url)
+const __dirname=path.dirname(__filename)
+
+
+app.get("/",(req,res)=>{
+// res.json({data:"Hello world from socket"})
+res.sendFile(__dirname + '/index.html');
+});
+
+
+io.on("connection",(socket)=>{
+    console.log("Connection is ready")
+})
+
+
+httpServer.listen(PORT,()=>{
+    console.log("Server is running at http://localhost:4000");
+})
